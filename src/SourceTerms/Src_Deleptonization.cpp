e7c985b1cc3b9db13f8e221bf2313b0a184db9fd
K.-C. Pan <pankuoch@msu.edu>
2018-12-18 12:50:29 +0800

    WIP: deleptonization

--- src/SourceTerms/Src_Deleptonization.cpp
+++ src/SourceTerms/Src_Deleptonization.cpp
@@ -1,4 +1,5 @@
 #include "GAMER.h"
+#include "NuclearEos.h"
 
 // declare as static so that other functions cannot invoke it directly and must use the function pointer
 static void Src_Deleptonization( real fluid[], const double x, const double y, const double z, const double Time,
@@ -7,6 +8,7 @@ static void Src_Deleptonization( real fluid[], const double x, const double y, c
 void (*Src_Deleptonization_Ptr)( real fluid[], const double x, const double y, const double z, const double Time,
                       const int lv, double AuxArray[], const double dt ) = Src_Deleptonization;
 
+double YeOfRhoFunc(double);
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  Src_Deleptonization
@@ -42,7 +44,77 @@ void Src_Deleptonization( real fluid[], const double x, const double y, const do
    fluid[ENGY] = Ek + Eint;
    */
 
+   const double delep_minDens = 1.e6; // [g/cm^3]
+   const double mev_to_kelvin = 1.1604447522806e10;
+   const double Q = 1.293333;
 
-   printf("delep: %13.7e\n",DELEP_RHO1);
+   double xdens,dens, ener, entr, ye, xmom, ymom, zmom;
+   double del_ye, del_entr;
+   double xtmp, xenr, xprs, xent, xcs2, xdedt, xdpderho, xdpdrhoe, xmunu, rfeps;
+   int keyerr;
+
+   double yout;
+
+   dens  = fluid[DENS]; // code units
+   xdens = dens*UNIT_D;  // [g/cm^3]
+   entr  = fluid[ENTR];
+   xmom  = fluid[MOMX]*UNIT_V;
+   ymom  = fluid[MOMY]*UNIT_V;
+   zmom  = fluid[MOMZ]*UNIT_V;
+   ye    = fluid[YE];
+   ener  = fluid[ENGY];
+   xenr = ener - energy_shift - 0.5*( SQR(fluid[MOMX]) + SQR(fluid[MOMY]) + SQR(fluid[MOMZ]) )/dens;
+
+   del_ye = 0.0;
+   if (xdens <= delep_minDens)
+   {
+     del_ye = 0.0;
+   } else
+   {
+     yout = YeOfRhoFunc(xdens);
+     del_ye = yout - ye;
+     del_ye = MIN(0.0, del_ye); // Deleptonization cannot increase Ye
+   }
+
+   if (del_ye < 0.0)
+   {
+      nuc_eos_C_short(xdens,&xtmp,ye,&xenr, &xprs, &xent, &xcs2, &xdedt, &xdpderho,
+          &xdpdrhoe, &xmunu, 0, &keyerr, rfeps); // energy mode
+
+      xmunu += Q;
+
+      if ((xmunu < DELEP_ENU) || (xdens > 2.e12))
+      {
+        del_entr = 0.0;
+      } else
+      {
+        del_entr = - del_ye * (xmunu - DELEP_ENU) / xtmp;
+      }
+
+      fluid[ENTR] = entr + del_entr;
+      fluid[YE]   = ye + del_ye;
+
+      nuc_eos_C_short(xdens,&xtmp,ye,&xenr, &xprs, &xent, &xcs2, &xdedt, &xdpderho,
+          &xdpdrhoe, &xmunu, 2, &keyerr, rfeps); // entropy mode
+
+      fluid[ENGY] = (xenr + energy_shift) + 0.5*( SQR(fluid[MOMX]) + SQR(fluid[MOMY]) + SQR(fluid[MOMZ]) ) / fluid[DENS];
+   }
+
+
+
+   //printf("delep: del_ye %13.7e\n",del_ye);
+   //printf("delep: dye %13.7e\n",(yout-ye));
 
 } // FUNCTION : Src_User
+
+double YeOfRhoFunc(double xdens)
+{
+  double xofrho, xofrho2, ye;
+  xofrho = 2.0*log10(xdens) - log10(DELEP_RHO2) - log10(DELEP_RHO1);
+  xofrho = xofrho / (log10(DELEP_RHO2) - log10(DELEP_RHO1));
+  xofrho = MAX(-1.0, MIN(1.0, xofrho));
+  ye = 0.5*(DELEP_YE2 + DELEP_YE1) + 0.5*xofrho*(DELEP_YE2 - DELEP_YE1);
+  ye = ye + DELEP_YEC*(1.0 - fabs(xofrho));
+  ye = ye + 4.0*fabs(xofrho)*(fabs(xofrho)-0.5)*(fabs(xofrho) - 1.0);
+  return ye;
+}
