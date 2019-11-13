b7ab992bbeb3b20487128819154a671a15825781
He-Feng Hsieh <x.geometric@gmail.com>
2019-11-13 13:33:54 +0800

    Support multiple profiles

--- src/SelfGravity/CPU_Poisson/CPU_ComputeEffPot.cpp
+++ src/SelfGravity/CPU_Poisson/CPU_ComputeEffPot.cpp
@@ -48,10 +48,15 @@ void CPU_ComputeEffPot()
    const double MinBinSize = ( GREP_MinBinSize > 0.0 ) ? GREP_MinBinSize
                                                        : amr->dh[MAX_LEVEL];
 
-   Aux_ComputeProfile( &DensAve, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, DENS          );
-   Aux_ComputeProfile( &EngyAve, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, INTERNAL_ENGY );
-   Aux_ComputeProfile( &VrAve,   Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, VRAD          );
-   Aux_ComputeProfile( &PresAve, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, PRESSURE      );
+   Profile_t *Prof_all = { &DensAve, &EngyAve,      &VrAve, &PresAve };
+   const int  Quantity = {  DENS,     INTERNAL_ENGY, VRAD,   PRESSURE};
+
+   Aux_ComputeProfile( Prof_all, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, Quantity );
+
+//   Aux_ComputeProfile( &DensAve, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, DENS          );
+//   Aux_ComputeProfile( &EngyAve, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, INTERNAL_ENGY );
+//   Aux_ComputeProfile( &VrAve,   Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, VRAD          );
+//   Aux_ComputeProfile( &PresAve, Center, MaxRadius, MinBinSize, GREP_LogBin, GREP_LogBinRatio, true, PRESSURE      );
 
 
 // iteratively construct the m_TOV and Gamma
